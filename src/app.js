cf6f466d3b9ed8568dab4cf41598f28197f6f00d
rajeevkumar2310 <rajeevkumar.gorikapudi@gmail.com>
2024-10-22 06:39:40 +0530

    exploring postman and get put post

--- src/app.js
+++ src/app.js
@@ -2,8 +2,28 @@ const express = require("express");
 
 const app = express();
 
-app.use((req, res) => {
-  res.send("Hello from the server");
+app.get("/user", (req, res) => {
+  res.send("This is a get request.");
+});
+
+app.post("/user", (req, res) => {
+  res.send("data saved to the database successfully");
+});
+
+app.delete("/user", (req, res) => {
+  res.send("data deleted successfully");
+});
+
+app.use("/test", (req, res) => {
+  res.send("this is a test page");
+});
+
+app.use("/hello", (req, res) => {
+  res.send("Hello from hello page");
+});
+
+app.use("/", (req, res) => {
+  res.send("Hello from___________________home page");
 });
 
 app.listen(7777, () => {
