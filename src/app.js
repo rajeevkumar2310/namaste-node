bd9f1333a9e77b6c5dcbf0e7e957a692a82bd967
rajeevkumar2310 <rajeevkumar.gorikapudi@gmail.com>
2024-10-24 06:01:29 +0530

    played with res.send and next and arrays

--- src/app.js
+++ src/app.js
@@ -31,39 +31,73 @@ const app = express();
 //   res.send({ fname: "Rajeev", lname: "Kumar" });
 // });
 
-app.get("/user/:userid/:fname/:pwd", (req, res) => {
-  console.log(req.params);
-  res.send({ fname: "Rajeev", lname: "Kumar" });
-});
+// app.get("/user/:userid/:fname/:pwd", (req, res) => {
+//   console.log(req.params);
+//   res.send({ fname: "Rajeev", lname: "Kumar" });
+// });
 
-app.get("/user", (req, res) => {
-  console.log(req.query);
-  res.send("This will show query params");
-});
+// app.get("/user", (req, res) => {
+//   console.log(req.query);
+//   res.send("This will show query params");
+// });
 
-app.get("/user", (req, res) => {
-  res.send("This is a get request.");
-});
+// app.get("/user", (req, res) => {
+//   res.send("This is a get request.");
+// });
 
-app.post("/user", (req, res) => {
-  res.send("data saved to the database successfully");
-});
+// app.post("/user", (req, res) => {
+//   res.send("data saved to the database successfully");
+// });
 
-app.delete("/user", (req, res) => {
-  res.send("data deleted successfully");
-});
+// app.delete("/user", (req, res) => {
+//   res.send("data deleted successfully");
+// });
 
-app.use("/test", (req, res) => {
-  res.send("this is a test page");
-});
+// app.use("/test", (req, res) => {
+//   res.send("this is a test page");
+// });
 
-app.use("/hello", (req, res) => {
-  res.send("Hello from hello page");
-});
+// app.use("/hello", (req, res) => {
+//   res.send("Hello from hello page");
+// });
 
-app.use("/", (req, res) => {
-  res.send("Hello from___________________home page");
-});
+// app.use("/", (req, res) => {
+//   res.send("Hello from___________________home page");
+// });
+
+app.use("/user2", [
+  (req, res, next) => {
+    console.log("This is 1st request handler");
+    next();
+    // res.send("1st response");
+  },
+  (req, res, next) => {
+    console.log("This is 2nd request handler");
+    // res.send("2nd response");
+    next();
+  },
+  [
+    (req, res, next) => {
+      console.log("this is 3rd request handler");
+      // res.send("3rd response");
+      next();
+    },
+    (req, res, next) => {
+      console.log("this is 4th request handler");
+      // res.send("4th response");
+      next();
+    },
+  ],
+  [
+    [
+      (req, res, next) => {
+        console.log("this is 5th request handler");
+        res.send("5th response");
+        // next();
+      },
+    ],
+  ],
+]);
 
 app.listen(7777, () => {
   console.log("Server is listening on port 7777");
